8bf79dbcac87c841da3e84aaa967e5def210c90b
Mouhamadou-Soumare <82391111+Mouhamadou-Soumare@users.noreply.github.com>
2022-10-12 00:27:13 +0200

    Update HomeController.php

--- src/src/Controller/HomeController.php
+++ src/src/Controller/HomeController.php
@@ -17,17 +17,4 @@ class HomeController extends AbstractController
       }
 
 
-    /**
-       * Page d'accès à un article
-       * 
-       * @Route('/article/{iD}', name='show-article')
-       */
-      public function show($articleId)
-      {
-          // Nous retrouvons la valeur de la variable $articleId à partir de l'URI
-          // Par exemple /article/1234 => $articleId = '1234'
-
-          return new Response(" Voici le contenu de l'article avec l'ID $articleId ");
-      }
-
 }
